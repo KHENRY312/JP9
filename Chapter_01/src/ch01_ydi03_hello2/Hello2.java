7cdd941d6f6c9ee3a65f9690035b8c6e56b95c5a
KH312 <kimhenry312@gmail.com>
2021-02-01 09:28:17 -0500

    JP9-6 YDI_03 Hello2

---
+++ Chapter_01/src/ch01_ydi03_hello2/Hello2.java
@@ -0,0 +1,22 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ydi03_hello2;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ydi03_hello2
+File:     Hello2
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class Hello2
+  {
+    public static void main(String[] arg)
+      {
+        System.out.println("Hello, world!");
+      }
+  }
