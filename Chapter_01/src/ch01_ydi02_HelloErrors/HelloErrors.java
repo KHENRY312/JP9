146ba1f6c2dc0d7d2c1018da5482fff4e9e9da57
KH312 <kimhenry312@gmail.com>
2021-02-01 09:22:27 -0500

    JP9-5 YDI_02 HelloErrors

---
+++ Chapter_01/src/ch01_ydi02_HelloErrors/HelloErrors.java
@@ -0,0 +1,23 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ydi02_HelloErrors;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ydi02_HelloErrors
+File:     HelloErrors
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class HelloErrors
+  {
+    public static void main(String[] arg)
+      {
+        System.out.println("Hello");
+        System.out.println("This is a test.");
+      }
+  }
