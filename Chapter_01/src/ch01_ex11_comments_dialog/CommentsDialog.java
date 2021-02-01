7d123e8dfffe54cd57a347f7e5a49c2f54ff8e13
KH312 <kimhenry312@gmail.com>
2021-02-01 12:15:07 -0500

    JP9-14 EX11 CommentsDialog

--- Chapter_01/src/ch01_ex11_comments_dialog/CommentsDialog.java
+++ Chapter_01/src/ch01_ex11_comments_dialog/CommentsDialog.java
@@ -1,4 +1,4 @@
-/***************************************************************************************************
+/* *************************************************************************************************
  Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
   or reproduced for other uses than educational; and not to be used in lieu of performing your
   own assignments.
