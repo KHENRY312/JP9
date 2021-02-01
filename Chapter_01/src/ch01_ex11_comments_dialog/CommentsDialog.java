13d9306429c66d60f80059c32621877a88dcbff0
KH312 <kimhenry312@gmail.com>
2021-02-01 11:05:29 -0500

    JP9-14 EX11 CommentsDialog

---
+++ Chapter_01/src/ch01_ex11_comments_dialog/CommentsDialog.java
@@ -0,0 +1,31 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ex11_comments_dialog;
+import javax.swing.*;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ex11_comments_dialog
+File:     CommentsDialog
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class CommentsDialog
+  {
+    public static void main(String[] args)
+      {
+        JOptionPane.showMessageDialog(null, "Program comments are non-executing " +
+                "\nstatements you add to a file for " +
+                "\nthe purpose of documentation.",
+            "COMMENTS", JOptionPane.PLAIN_MESSAGE);
+      }
+  }
+	/*Program comments are non executing statements you add to a file for the purpose of
+	documentation.*/
+
+//Program comments are non executing statements you add to a file for the purpose of
+//documentation.
