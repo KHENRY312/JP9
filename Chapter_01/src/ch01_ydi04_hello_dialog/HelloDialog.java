ce88def5d5b6576fd696c3350741b3edb4b09c66
KH312 <kimhenry312@gmail.com>
2021-02-01 09:37:23 -0500

    JP9-7 YDI_04 HelloDialog

---
+++ Chapter_01/src/ch01_ydi04_hello_dialog/HelloDialog.java
@@ -0,0 +1,23 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ydi04_hello_dialog;
+import javax.swing.*;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ydi04_hello_dialog
+File:     HelloDialog
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class HelloDialog
+  {
+    public static void main(String[] arg)
+      {
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+      }
+  }
