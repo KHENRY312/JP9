5ca8c5313b72e49b66338d7e884c878840b783cc
KH312 <kimhenry312@gmail.com>
2021-02-01 10:04:44 -0500

    JP9-10 EX07 MovieQuoteInfo

---
+++ Chapter_01/src/ch01_ex07_movie_quote_info/MovieQuoteInfo.java
@@ -0,0 +1,28 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ex07_movie_quote_info;
+import javax.swing.*;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ex07_movie_quote_info
+File:     MovieQuoteInfo
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class MovieQuoteInfo
+  {
+    public static void main(String[] arg)
+      {
+        JOptionPane.showMessageDialog (null, "Gone with the Wind " +
+                "\nYear:  1939" +
+                "\nFrankly my dear, I dont give a damn" +
+                "\nRhett Butler",
+            "FAVORITE MOVIE QUOTE INFO",
+            JOptionPane.INFORMATION_MESSAGE);
+      }
+  }
