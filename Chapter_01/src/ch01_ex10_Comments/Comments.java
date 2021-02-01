3d8018ad52818f51a5d4c45791903a92da132d9e
KH312 <kimhenry312@gmail.com>
2021-02-01 10:54:36 -0500

    JP9-13 EX10 Comments

---
+++ Chapter_01/src/ch01_ex10_Comments/Comments.java
@@ -0,0 +1,28 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ex10_Comments;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ex10_Comments
+File:     Comments
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class Comments
+  {
+    public static void main (String[] arg)
+      {
+        System.out.println("Program comments are non-executing statements " +
+            "\nyou add to a file for the purpose of documentation.");
+      }
+  }
+	/*Program comments are non-executing statements you add to a file for the purpose of
+	documentation.*/
+
+/*Program comments are non-executing statements you add to a file for the purpose of
+documentation.*/
