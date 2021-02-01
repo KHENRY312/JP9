51ea62e97098d9568767290e5734ff698df1d3e8
KH312 <kimhenry312@gmail.com>
2021-02-01 10:17:46 -0500

    JP9-11 EX08 TableAndChairs

---
+++ Chapter_01/src/ch01_ex08_table_and_chairs/TableAndChairs.java
@@ -0,0 +1,34 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ex08_table_and_chairs;
+import javax.swing.*;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ex08_table_and_chairs
+File:     TableAndChairs
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class TableAndChairs
+  {
+    public static void main(String[] arg)
+      {
+        JOptionPane.showMessageDialog (null,
+            " "+
+                "\n**                                                              **" +
+                "\n**                                                              **" +
+                "\n**                                                              **" +
+                "\n************                             ************"+
+                "\n**            **   ** ************** **             **"+
+                "\n**            **   **                    **  **             **"+
+                "\n**            **   **                    **  **             **"+
+                "\n**            **   **                    **  **             **",
+            "Table And Chairs",
+            JOptionPane.INFORMATION_MESSAGE);
+      }
+  }
