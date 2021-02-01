2455b64efd259382affcd067874a17275c9c5601
KH312 <kimhenry312@gmail.com>
2021-02-01 11:12:03 -0500

    JP9-15 EX12 BurmaShave

---
+++ Chapter_01/src/ch01_ex12_burma_shave/BurmaShave.java
@@ -0,0 +1,30 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch01_ex12_burma_shave;
+import javax.swing.*;
+/*
+Project:  JP9
+Module:   Chapter_01
+Package:  ch01_ex12_burma_shave
+File:     BurmaShave
+Creator:  Kimberly Henry
+Created:  Mon, 01-Feb-2021
+*/
+public class BurmaShave
+  {
+    public static void main(String[] arg)
+      {
+        JOptionPane.showMessageDialog(null, "Approached a crossing,",
+            "BURMA SHAVE", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Without looking,",
+            "BURMA SHAVE", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Who will eat,",
+            "BURMA SHAVE", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "His widow's cooking.",
+            "BURMA SHAVE", JOptionPane.PLAIN_MESSAGE);
+      }
+  }
