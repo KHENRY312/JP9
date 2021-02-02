f645c799284e1989eec32d448d89d899917e714d
KH312 <kimhenry312@gmail.com>
2021-02-02 05:19:19 -0500

    JP9-20 DB04 Debugging Exercise OTHER 0h 6m (time spent on task)

---
+++ Chapter_01/src/ch01_db04_fix_debug_one4/FixDebugOne4.java
@@ -0,0 +1,9 @@
+package ch01_db04_fix_debug_one4;
+import javax.swing.JOptionPane;
+public class FixDebugOne4
+{
+   public static void main(String[] args)
+   {
+      JOptionPane.showMessageDialog(null, "First GUI program");
+   }
+}
