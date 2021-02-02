b1c0b056fd3f976344877cbd594f6f7ef1fd36b9
KH312 <kimhenry312@gmail.com>
2021-02-02 04:43:19 -0500

    JP9-17 DB01 Debugging Exercise

---
+++ Chapter_01/src/ch01_db01_fix_debug_one1/FixDebugOne1.java
@@ -0,0 +1,11 @@
+package ch01_db01_fix_debug_one1;
+public class FixDebugOne1
+{
+   /* This program displays a greeting
+   public void main(String[] args)
+   */
+  public static void main (String []args)
+   {
+      System.out.println("Hello");
+   }
+}
