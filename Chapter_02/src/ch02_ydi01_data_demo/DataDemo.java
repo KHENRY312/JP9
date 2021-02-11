13e9d0343c7fb9a923d76e4d50d9eec92d9397b7
Kimberly <kimberly.henry@outlook.com>
2021-02-11 18:34:44 -0500

    JP9-99 CH02_YDI-01 Declaring and Using a Variable OTHER 0h 0m | (time spent on task)

---
+++ Chapter_02/src/ch02_ydi01_data_demo/DataDemo.java
@@ -0,0 +1,27 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ ***************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_02
+Package:       ch02_ydi01_declaring_and_using_a_variable
+File:              DataDemo
+Creator:        Kimberly Henry
+Created on:   Thu, 11-Feb-2021
+Created at:    4:05 AM
+*/
+package ch02_ydi01_data_demo;
+
+public class DataDemo
+   {
+      public static void main(String[] args)
+         {
+            int aWholeNumber = 315;
+            System.out.print("The number is ");
+            System.out.println(aWholeNumber);
+            System.exit(0);
+         }
+   }
