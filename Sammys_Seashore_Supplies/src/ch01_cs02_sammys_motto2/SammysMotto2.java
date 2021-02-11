5ffbd8284ce3fe157ed48e6647ee910d54c75558
Kimberly <kimberly.henry@outlook.com>
2021-02-11 18:37:04 -0500

    JP9-98 CS02.02 SammysMotto2 OTHER 0h 0m | (time spent on task)

---
+++ Sammys_Seashore_Supplies/src/ch01_cs02_sammys_motto2/SammysMotto2.java
@@ -0,0 +1,26 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ ***************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Sammys_Seashore_Supplies
+Package:       ch01_cs02_sammys_motto2
+File:              SammysMotto2
+Creator:        Kimberly Henry
+Created on:   Thu, 11-Feb-2021
+Created at:    2:54 AM
+*/
+package ch01_cs02_sammys_motto2;
+
+public class SammysMotto2
+   {
+      public static void main(String[] args)
+         {
+            System.out.println("********************************");
+            System.out.println("Sammy’s makes it fun in the sun.");
+            System.out.println("********************************");
+         }
+   }
