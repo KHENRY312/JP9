497f9c4fdee417fb8bc16fdf52f68c3f0ee697a7
Kimberly <kimberly.henry@outlook.com>
2021-02-11 02:21:46 -0500

    JP9-96 CS01.02 CarlysMotto2 OTHER 0h 0m | (time spent on task)

---
+++ Carlys_Catering/src/ch01_cs02_carlys_motto2/CarlysMotto2.java
@@ -0,0 +1,26 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ ***************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Carlys_Catering
+Package:       ch01_cs02_carlys_motto2
+File:              CarlysMotto2
+Creator:        Kimberly Henry
+Created on:   Thu, 11-Feb-2021
+Created at:    2:02 AM
+*/
+package ch01_cs02_carlys_motto2;
+
+public class CarlysMotto2
+   {
+      public static void main(String[] args)
+         {
+            System.out.println("******************************************");
+            System.out.println("Carly’s makes the food that makes it a party.");
+            System.out.println("******************************************");
+         }
+   }
