3f1057ca4e9a669c29708b3c6b75f4afb1f90d18
Kimberly <kimberly.henry@outlook.com>
2021-02-13 06:26:59 -0500

    JP9-99 CH02_YDI-01 Declaring and Using a Variable OTHER 0h 0m | (time spent on task)

--- Chapter_02/src/ch02_ydi01_data_demo/DataDemo.java
+++ Chapter_02/src/ch02_ydi01_data_demo/DataDemo.java
@@ -19,9 +19,11 @@ public class DataDemo
    {
       public static void main(String[] args)
          {
+            final int STATES_IN_US = 50;
             int aWholeNumber = 315;
             System.out.print("The number is ");
-            System.out.println(aWholeNumber);
+            System.out.println(aWholeNumber + ".");
+            System.out.println("The number of states is " + STATES_IN_US + ".");
             System.exit(0);
          }
    }
