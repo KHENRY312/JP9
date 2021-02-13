a129e43f058940103cca642ef8562390b7aae786
Kimberly <kimberly.henry@outlook.com>
2021-02-13 07:05:30 -0500

    JP9-101 CH02_YDI-03 Working with the char Data Type OTHER 0h 8m |

---
+++ Chapter_02/src/ch02_ydi03_char_demo/CharDemo.java
@@ -0,0 +1,26 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ ***************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_02
+Package:       ch02_ydi03_char_demo
+File:              CharDemo
+Creator:        Kimberly Henry
+Created on:   Sat, 13-Feb-2021
+Created at:    6:59 AM
+*/
+package ch02_ydi03_char_demo;
+
+public class CharDemo
+   {
+      public static void main(String[] args)
+         {
+            char initial = 'A';
+            System.out.println(initial);
+            System.out.print("\t\"abc\\def\bghi\n\njkl");
+         }
+   }
