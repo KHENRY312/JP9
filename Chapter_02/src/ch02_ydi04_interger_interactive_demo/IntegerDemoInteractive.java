8347931edcf9a119b97c7a35b2ddd96d01453fe1
Kimberly <kimberly.henry@outlook.com>
2021-02-15 09:12:57 -0500

    JP9-102 CH02_YDI-04 Accepting User Input OTHER 0h 48m | (time spent on task)

---
+++ Chapter_02/src/ch02_ydi04_interger_interactive_demo/IntegerDemoInteractive.java
@@ -0,0 +1,41 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ **********************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_02
+Package:       ch02_ydi04_integer_interactive_demo
+File:              IntegerDemoInteractive
+Creator:        Kimberly Henry
+Created on:   Sat, 13-Feb-2021
+Created at:    8:23 PM
+*/
+package ch02_ydi04_interger_interactive_demo;
+
+import java.util.Scanner;
+
+public class IntegerDemoInteractive
+   {
+      public static void main(String[] args)
+         {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Please enter an integer >>  ");
+            int anInt = keyboard.nextInt();
+            System.out.print("Please enter a byte integer >>  ");
+            byte aByte = keyboard.nextByte();
+            System.out.print("Please enter a short integer >>  ");
+            short aShort = keyboard.nextShort();
+            System.out.print("Please enter a long integer >> ");
+            long aLong = keyboard.nextLong();
+            System.out.println("The int is " + anInt + ".");
+            System.out.println("The byte is " + aByte + ".");
+            System.out.println("The short is " + aShort + ".");
+            System.out.println("The long is " + aLong + ".");
+            int anotherInt =  anInt * 1000000;
+            System.out.println("anotherInt is " + anotherInt + ".");
+
+         }
+   }
