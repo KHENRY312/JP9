0b33dd9f7329048b5be3c6eebd76b006e0e09789
Kimberly <kimberly.henry@outlook.com>
2021-02-17 02:43:01 -0500

    JP9-105 CH02_YDI-07 Performing Floating-Point Arithmetic OTHER 0h 10m |Performing Floating-Point Arithmetic

---
+++ Chapter_02/src/ch02_ydi07_arithmetic_demo2/ArithmeticDemo2.java
@@ -0,0 +1,41 @@
+/* *************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ **************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_02
+Package:       ch02_ydi07_arithmetic_demo2
+File:              ArithmeticDemo2
+Creator:        Kimberly Henry
+Created on:   Wed, 17-Feb-2021
+Created at:    2:30 AM
+*/
+package ch02_ydi07_arithmetic_demo2;
+
+import java.util.Scanner;
+
+public class ArithmeticDemo2
+   {
+      public static void main(String[] args)
+         {
+            double firstNumber;
+            double secondNumber;
+            double sum;
+            double difference;
+            double average;
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Please enter a double >> ");
+            firstNumber = keyboard.nextDouble();
+            System.out.print("Please enter another double >> ");
+            secondNumber = keyboard.nextDouble();
+            sum = firstNumber + secondNumber;
+            difference = firstNumber - secondNumber;
+            average = sum / 2;
+            System.out.println(firstNumber + " + " + secondNumber + " is " + sum);
+            System.out.println(firstNumber + " - " + secondNumber + " is " + difference);
+            System.out.println("The average of " + firstNumber + " and " + secondNumber + " is " + average);
+         }
+   }
