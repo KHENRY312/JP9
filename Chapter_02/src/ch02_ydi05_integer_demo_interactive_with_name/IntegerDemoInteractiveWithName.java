035cb35b4418dc7e3ad52955c22d314815c34ebc
Kimberly <kimberly.henry@outlook.com>
2021-02-16 23:32:09 -0500

    JP9-21 CH02 You Do It OTHER 0h 34m | (time spent on task)

--- Chapter_02/src/ch02_ydi05_integer_demo_interactive_with_name/IntegerDemoInteractiveWithName.java
+++ Chapter_02/src/ch02_ydi05_integer_demo_interactive_with_name/IntegerDemoInteractiveWithName.java
@@ -25,6 +25,20 @@ public class IntegerDemoInteractiveWithName
             String name;
             System.out.print("Please enter your name >>  ");
             name = keyboard.nextLine();
+            System.out.print("Please enter an integer >>  ");
+            int anInt = keyboard.nextInt();
+            System.out.print("Please enter a byte integer >>  ");
+            byte aByte = keyboard.nextByte();
+            System.out.print("Please enter a short integer >>  ");
+            short aShort = keyboard.nextShort();
+            System.out.print("Please enter a long integer >> ");
+            long aLong = keyboard.nextLong();
+            System.out.println("The int is " + anInt + ".");
+            System.out.println("The byte is " + aByte + ".");
+            System.out.println("The short is " + aShort + ".");
+            System.out.println("The long is " + aLong + ".");
+            int anotherInt =  anInt * 1000000;
+            System.out.println("anotherInt is " + anotherInt + ".");
             System.out.println("Thank you, " + name);
          }
    }
