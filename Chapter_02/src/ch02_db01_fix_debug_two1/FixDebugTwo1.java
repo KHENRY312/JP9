6813c95efc942f6a938d706dec67fb3139d2190a
Kimberly <kimberly.henry@outlook.com>
2021-02-20 10:17:01 -0500

    JP9-122 CH02_DB01 FixDebugTwo1 OTHER 0h 1m |

---
+++ Chapter_02/src/ch02_db01_fix_debug_two1/FixDebugTwo1.java
@@ -0,0 +1,23 @@
+/* ************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+package ch02_db01_fix_debug_two1;
+
+public class FixDebugTwo1
+{
+   public static void main(String[] args)
+   {
+      int oneInt = 315;
+      double oneDouble = 12.4;
+      char oneChar = 'A';
+      System.out.print("The int is ");
+      System.out.println(oneInt);
+      System.out.print("The double is ");
+      System.out.println(oneDouble);
+      System.out.print("The char is ");
+      System.out.println(oneChar);
+   }
+}
