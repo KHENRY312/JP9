181c4a7da43fe0c5c6630e923b9ea15a21d234e8
Kimberly <kimberly.henry@outlook.com>
2021-02-19 20:40:42 -0500

    JP9-115 CH02_EX10 Initials OTHER 0h 8m | (time spent on task)

---
+++ Chapter_02/src/ch02_ex10_Initials/Initials.java
@@ -0,0 +1,28 @@
+/* *************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ **************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_02
+Package:       ch02_ex10_Initials
+File:              Initials
+Creator:        Kimberly Henry
+Created on:   Fri, 19-Feb-2021
+Created at:    8:31 PM
+*/
+package ch02_ex10_Initials;
+
+public class Initials
+   {
+      public static void main(String[] args)
+         {
+           char first =  'K';
+           char last = 'H';
+           char period = '.';
+           String space = " ";
+           System.out.print("My initials are " + first + period + space + last + period);
+         }
+   }
