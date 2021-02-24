f8f353e22656652f38d36ba13dfc8342e6c98d80
Kimberly <kimberly.henry@outlook.com>
2021-02-23 20:25:56 -0500

    JP9-134 CH01 RandomGuess 0h 14m |

---
+++ Game_Zone/src/ch01_random_guess/RandomGuess.java
@@ -0,0 +1,30 @@
+/* *************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ **************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Game_Zone
+Package:       ch01_random_guess
+File:              RandomGuess
+Creator:        Kimberly Henry
+Created on:   Tue, 23-Feb-2021
+Created at:    8:11 PM
+*/
+package ch01_random_guess;
+
+import javax.swing.*;
+
+public class RandomGuess
+   {
+      public static void main(String[] args)
+         {
+            JOptionPane.showMessageDialog(null, "Think of a number between 1 and 10.",
+                  "GUESS NUMBER",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "The number is " + (1 + (int) (Math.random() * 10) + "."),
+                  "RANDOM NUMBER", JOptionPane.INFORMATION_MESSAGE);
+
+         }
+   }
