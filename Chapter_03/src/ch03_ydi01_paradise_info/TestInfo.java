ac897f3e992b88378b6e19d28caec19fc09a72fd
Kimberly <kimberly.henry@outlook.com>
2021-02-28 07:17:27 -0500

    ch03_ydi01_paradise_info

---
+++ Chapter_03/src/ch03_ydi01_paradise_info/TestInfo.java
@@ -0,0 +1,25 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ **************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_03
+Package:       ch03_ydi01_paradise_info
+File:              TestInfo
+Creator:        Kimberly Henry
+Created on:   Sun, 28-Feb-2021
+Created at:    7:05 AM
+*/
+package ch03_ydi01_paradise_info;
+
+public class TestInfo
+   {
+      public static void main(String[] args)
+         {
+            System.out.println("Calling method from another class:");
+            ParadiseInfo.displayInfo();
+         }
+   }
