a23826bc7cc9d5ac07a57b87a64ed7c757f9ca98
Kimberly <kimberly.henry@outlook.com>
2021-02-28 07:18:59 -0500

    ch03_ydi01_paradise_info

---
+++ Chapter_03/src/ch03_ydi01_paradise_info/ParadiseInfo.java
@@ -0,0 +1,29 @@
+/* **************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+ or reproduced for other uses than educational; and not to be used in lieu of performing your
+ own assignments.
+ **************************************************************************************************/
+
+/*
+Project:         JP9
+Module:        Chapter_03
+Package:       ch03_ydi01_paradise_info
+File:              ParadiseInfo
+Creator:        Kimberly Henry
+Created on:   Sun, 28-Feb-2021
+Created at:    6:59 AM
+*/
+package ch03_ydi01_paradise_info;
+
+public class ParadiseInfo
+   {
+      public static void main(String[] args)
+         {
+           displayInfo();
+         }
+      public static void displayInfo()
+         {
+            System.out.println("Paradise Day Spad wants to pamper you.");
+            System.out.println("We will make you look good.");
+         }
+   }
