f680b63a67cb13f9b345a744d987f92476579e24
Kim <kimhenry312@gmail.com>
2021-03-23 07:38:34 -0400

    Cleanup Commits

--- Chapter_01/src/ch01_db01_fix_debug_one1/FixDebugOne1.java
+++ Chapter_01/src/ch01_db01_fix_debug_one1/FixDebugOne1.java
@@ -1,3 +1,9 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
 package ch01_db01_fix_debug_one1;
 public class FixDebugOne1
 {
--- Chapter_01/src/ch01_db02_fix_debug_one2/FixDebugOne2.java
+++ Chapter_01/src/ch01_db02_fix_debug_one2/FixDebugOne2.java
@@ -1,14 +1,15 @@
 package ch01_db02_fix_debug_one2;
 
 public class FixDebugOne2
-{
-   /* This program displays some output*/
-   public static void main(String []args)
    {
-      System.out.println("Java programming is fun.");
-      System.out.println("Getting a program to work");
-      System.out.println("can be a challenge,");
-      System.out.println("but when everything works correctly,");
-      System.out.println("it's very satisfying");
+      /* This program displays some output*/
+      public static void main(String[] args)
+         {
+            System.out.println("Java programming is fun.");
+            System.out.println("Getting a program to work");
+            System.out.println("can be a challenge,");
+            System.out.println("but when everything works correctly,");
+            System.out.println("it's very satisfying");
+            System.exit(0);
+         }
    }
-}
