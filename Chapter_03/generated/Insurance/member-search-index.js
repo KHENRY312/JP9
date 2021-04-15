a73d87978f4841110db8f518ee0dcf6f903ee44d
KH312 <kimhenry312@gmail.com>
2021-04-15 10:03:55 -0400

    JP9-109  ch03_ex09_insurance 0h 0m

---
+++ Chapter_03/generated/Insurance/member-search-index.js
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+memberSearchIndex = [{"p": "ch03_ex09_insurance", "c": "Insurance", "l": "calculatePremium(int, int)", "u": "calculatePremium(int,int)"}, {
+    "p": "ch03_ex09_insurance",
+    "c": "Insurance",
+    "l": "Insurance()",
+    "u": "%3Cinit%3E()"
+}, {"p": "ch03_ex09_insurance", "c": "Insurance", "l": "main(String[])", "u": "main(java.lang.String[])"}];
+updateSearchResults();
---
+++ Chapter_03/generated/Insurance/package-search-index.js
@@ -0,0 +1,8 @@
+/***************************************************************************************************
+ Copyright (c) 2021. Kimberly Henry This code shall not be used, manipulated, decompiled
+  or reproduced for other uses than educational; and not to be used in lieu of performing your
+  own assignments.
+ **************************************************************************************************/
+
+packageSearchIndex = [{"l": "All Packages", "u": "allpackages-index.html"}, {"l": "ch03_ex09_insurance"}];
+updateSearchResults();
