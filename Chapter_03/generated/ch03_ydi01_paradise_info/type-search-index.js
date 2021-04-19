b3f885434268c115f221152732a8cf94a2fa3ba6
KH312 <kimhenry312@gmail.com>
2021-04-19 08:18:32 -0400

    JP9-75  CH03_YDI01_paradise_info  0h 0m

---
+++ Chapter_03/generated/ch03_ydi01_paradise_info/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"ch03_ydi01_paradise_info","l":"ParadiseInfo"},{"p":"ch03_ydi01_paradise_info","l":"ParadiseInfo2"},{"p":"ch03_ydi01_paradise_info","l":"TestInfo"}];updateSearchResults();
